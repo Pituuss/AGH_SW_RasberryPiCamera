3157fd5870ce8d7adae79be21999e42a7e8696af
Piotr Wojtyś <pituuuss@gmail.com>
2019-01-15 14:13:27 +0100

    refactor

--- Project.py
+++ Project.py
@@ -27,54 +27,58 @@ insert_time = time.time() - feed_time
 edges = None
 offset = 20
 
-def classify_frame(inputQueue, outputQueue, sem,kill):
-	time.sleep(2.0)
-	t = threading.currentThread()
-	while getattr(t, "kill", True):
-		sem.wait()
-		frame = inputQueue.get()
-		face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-		faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50),maxSize=(250,250))
-		outputQueue.put(faces)
-		sem.clear()
 
-p = Thread(target=classify_frame,args=(inputQueue,outputQueue,sem,kill))
+def classify_frame(inputQueue, outputQueue, sem, kill):
+    time.sleep(2.0)
+    t = threading.currentThread()
+    while getattr(t, "kill", True):
+        sem.wait()
+        frame = inputQueue.get()
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50),
+                                              maxSize=(250, 250))
+        outputQueue.put(faces)
+        sem.clear()
+
+
+p = Thread(target=classify_frame, args=(inputQueue, outputQueue, sem, kill))
 p.start()
 
 while True:
-	frame = vs.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	if not sem.is_set() and time.time() - insert_time > feed_time:
-		insert_time = time.time()
-		inputQueue.put(gray)
-		sem.set()
+    frame = vs.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if not sem.is_set() and time.time() - insert_time > feed_time:
+        insert_time = time.time()
+        inputQueue.put(gray)
+        sem.set()
 
-	if not outputQueue.empty():
-		faces = outputQueue.get() 
+    if not outputQueue.empty():
+        faces = outputQueue.get()
 
-	if initBB is not None:
-		(success, box) = tracker.update(gray)
-		if success:
-			(x, y, w, h) = [int(v) for v in box]
-			cv2.rectangle(frame, (max(x-offset,0),max(y-offset,0)), (x+w+offset, y+h+offset), (0, 255, 0), 2)
-			x0,x1,y0,y1 = (max(y-offset,0),y+h+offset,max(x-offset,0),x+w+offset)
-			edges = cv2.Canny(frame[x0:x1,y0:y1],50,50)
+    if initBB is not None:
+        (success, box) = tracker.update(gray)
+        if success:
+            (x, y, w, h) = [int(v) for v in box]
+            cv2.rectangle(frame, (max(x - offset, 0), max(y - offset, 0)), (x + w + offset, y + h + offset),
+                          (0, 255, 0), 2)
+            x0, x1, y0, y1 = (max(y - offset, 0), y + h + offset, max(x - offset, 0), x + w + offset)
+            edges = cv2.Canny(frame[x0:x1, y0:y1], 50, 50)
 
-	if faces is not None:
-		if len(faces) != 0:
-			initBB = tuple(faces[0])
-			tracker = cv2.TrackerMOSSE_create()
-			tracker.init(frame, initBB)
-			faces = None
-	
-	if edges is not None:
-		frame[x0:x1,y0:y1,0] = edges 
-		frame[x0:x1,y0:y1,1] = edges 
-	cv2.imshow('frame', frame)
+    if faces is not None:
+        if len(faces) != 0:
+            initBB = tuple(faces[0])
+            tracker = cv2.TrackerMOSSE_create()
+            tracker.init(frame, initBB)
+            faces = None
 
-	key = cv2.waitKey(1) & 0xff
-	if key == ord('q'):
-		break
+    if edges is not None:
+        frame[x0:x1, y0:y1, 0] = edges
+        frame[x0:x1, y0:y1, 1] = edges
+    cv2.imshow('frame', frame)
+
+    key = cv2.waitKey(1) & 0xff
+    if key == ord('q'):
+        break
 
 p.kill = False
 sem.set()
